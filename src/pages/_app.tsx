f1591bfb10e7b7ce311dd8b72609522a15fa270f
Nico Schett <nicoschett@icloud.com>
2021-04-28 01:55:52 +0200

    First draft

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '@common/logo.svg'
 
-function App() {
+import './_app.scss'
+
+const App = (): JSX.Element => {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,13 +14,12 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
